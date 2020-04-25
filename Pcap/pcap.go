ac3f650e25b61962b0f784c09ba98d35551e8731
JiHounHuh <jhuh009@ucr.edu>
2020-04-25 14:25:22 -0700

    Attempt 1 to write pcap writer

--- Pcap/pcap.go
+++ Pcap/pcap.go
@@ -2,12 +2,27 @@ package Pcap
 
 import (
 	"fmt"
-	//"github.com/google/gopacket"
-	//"github.com/google/gopacket/pcapgo"
-	//"github.com/google/gopacket/layers"
+//	"os"
+	"time"
+	"github.com/google/gopacket"
+//	"github.com/google/gopacket/pcapgo"
+//	"github.com/google/gopacket/layers"
 )
-
 
 func Demo (Dev string) {
 	fmt.Printf("%s\n",Dev)
+	// Open interface
+	if handle, err = pcap.OpenLive(Dev, 1600, true, 0); err != nil {
+		panic(err)
+	} else if err := handle.SetBPFFilter("tc amd prt 80"); err != nil {
+		panic(err)
+	} else { // Run if no errors
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSource.Packets() {
+			// Process packets here
+			fmt.Println(packet)
+		}
+	}
+	// Start capturing network traffic
+	//
 }
