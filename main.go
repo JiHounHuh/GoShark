1065bab6b0246fd291849aa0a18d8473835a7fc0
Anthony Hallak <ahall012@ucr.edu>
2020-04-25 11:26:42 -0700

    Added main.go, barebones

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+ )
+
+ func main() {
+	 fmt.Println("|:|:|:|:|:| GoShark |:|:|:|:|:|")
+	 fmt.Println("Available interfaces")
+	 // list off interfaces
+	 // have user choose 1 AND/OR 2,....
+	 // then call capture.go with that interface
+ }
