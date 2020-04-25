836e1276b68b29cbff797b0187c45e290aabcad3
Luis Garcia <luisgarcia@Luiss-MacBook-Pro.local>
2020-04-25 11:52:32 -0700

    listing interfaces

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net"
  )
+
+ func listInterfaces() {
+	 interfaces, err := net.Interfaces()
+	 if err != nil {
+		 fmt.Print(fmt.Errorf("listInterfaces: %+v\n", err.Error()))
+		 return
+	 }
+	 for index, iface := range interfaces {
+		 fmt.Println(index,": ",iface.Name)
+	 }
+ }
 
  func main() {
 	 fmt.Println("|:|:|:|:|:| GoShark |:|:|:|:|:|")
 	 fmt.Println("Available interfaces")
+	 listInterfaces()
 	 // list off interfaces
 	 // have user choose 1 AND/OR 2,....
 	 // then call capture.go with that interface
