e6fcfdb5ed0732350a463d850eb8d043dee2b0e5
JiHounHuh <25492217+JiHounHuh@users.noreply.github.com>
2020-04-25 12:30:11 -0700

    [Task] Add initial Pcap Files

---
+++ Pcap/pcap.go
@@ -0,0 +1,13 @@
+package Pcap
+
+import {
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"github.com/google/gopacket/pcapgo"
+	"github.com/google/gopacket/layers"
+}
+
+func Demo () {
+	fmt.Println("Hello")
+}
