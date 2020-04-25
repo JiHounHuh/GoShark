ac8d2b3b777f02957135005a8420cb295f45ce06
Anthony Hallak <ahall012@ucr.edu>
2020-04-25 14:39:18 -0700

    Fixed syntax stuff in pcap.go

--- Pcap/pcap.go
+++ Pcap/pcap.go
@@ -3,16 +3,16 @@ package Pcap
 import (
 	"fmt"
 //	"os"
-	"time"
+//	"time"
 	"github.com/google/gopacket"
-//	"github.com/google/gopacket/pcapgo"
+	"github.com/google/gopacket/pcap"
 //	"github.com/google/gopacket/layers"
 )
 
 func Demo (Dev string) {
 	fmt.Printf("%s\n",Dev)
 	// Open interface
-	if handle, err = pcap.OpenLive(Dev, 1600, true, 0); err != nil {
+	if handle, err := pcap.OpenLive(Dev, 1600, true, 0); err != nil {
 		panic(err)
 	} else if err := handle.SetBPFFilter("tc amd prt 80"); err != nil {
 		panic(err)
