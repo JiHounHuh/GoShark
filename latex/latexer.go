1954a5da158b2c9a8a410f9c4632aea9c5a5cac0
Anthony Hallak <ahall012@ucr.edu>
2020-04-26 01:24:18 -0700

    Started Latex Writer

---
+++ latex/latexer.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"io/ioutil"
+	"strings"
+)
+//\usepackage{fullpage,latexsym,picinpar,amsmath,amsfonts}
+func main() {
+	content := 
+`
+\documentclass{article}
+\usepackage{graphicx}
+\begin{document}
+\begin{center}
+	\includegraphics[scale=0.25]{Cyber_525x438.png}\\
+\end{center}	
+\centerline{\large \bf REPORT}
+\begin{center}
+ \begin{tabular}{||c c c c||} 
+ \hline 
+ SrcIP & DstIP & SrcPort & DstPort \\ [0.5ex]
+ \hline\hline
+`
+	end :=
+`
+\end{tabular}
+\end{center}
+\end{document}
+`
+
+/*
+
+
+
+\begin{center}
+ \begin{tabular}{||c c c c||} 
+ \hline
+ SrcIP & SrcPort & DstIP & DstPort \\ [0.5ex] 
+ \hline\hline
+ 1 & 6 & 87837 & 787 \\ 
+ \hline
+ 2 & 7 & 78 & 5415 \\
+ \hline
+ 3 & 545 & 778 & 7507 \\
+ \hline
+ 4 & 545 & 18744 & 7560 \\
+ \hline
+ 5 & 88 & 788 & 6344 \\ [1ex] 
+ \hline
+\end{tabular}
+\end{center}
+*/
+
+	// for loop to search file to add to latex
+	toRead, readErr := ioutil.ReadFile("toRead")
+
+	if readErr != nil {
+		fmt.Println("Error reading insecure packets from file", readErr)
+		os.Exit(1)
+	}
+
+	splits := strings.Fields(string(toRead))
+
+	var lineToAdd string = ""
+	for i, c := range splits {
+		fmt.Println("i",i)
+		lineToAdd += c // dstIP, srcIP, srcPort, dstPort
+		lineToAdd += " & "
+		fmt.Println("linesToAdd",lineToAdd)
+
+		toC := i+1
+
+		if toC % 4 == 0  {
+			fmt.Println("GOT ALL 4")
+			content += lineToAdd[0:len(lineToAdd)-2] 
+			content += " \\\\\n\\hline\n"
+			lineToAdd = ""
+		}
+	}
+
+
+	content += end
+
+	fmt.Println("To write:\n",content)
+	writeErr := ioutil.WriteFile("test.tex", []byte(content), 0644)
+
+	if writeErr != nil {
+		fmt.Println("Error writing file")
+		os.Exit(1)
+	}
+
+	cmd := exec.Command("/usr/bin/pdflatex","report.tex")
+	cmdErr := cmd.Run()
+
+	if cmdErr != nil {
+		fmt.Println("Error running pdflatex on report.tex",cmdErr)
+		//os.Exit(1)
+	}
+}
