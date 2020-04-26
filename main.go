c3adb11da05f483d520ad06b6a6b9c7683200e70
Luis Garcia <luisgarcia@Luiss-MacBook-Pro.local>
2020-04-26 03:49:03 -0700

    test

--- main.go
+++ main.go
@@ -70,7 +70,6 @@ func listInterfaces() []string {
 
 	 iface = listInterfaces()
 	 input := readUserInput(len(iface))
-	 fmt.Println(input)
 	 L.Capture(iface[input])
 	 // list off interfaces
 	 // have user choose 1 AND/OR 2,....
